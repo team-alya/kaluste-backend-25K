068823b7c986ede80c4a21b5524715a56eb66164
Santks <santeri.kuronen@myy.haaga-helia.fi>
2025-03-28 09:18:07 +0200

    backend role checking v1

--- src/routes/evaluation.ts
+++ src/routes/evaluation.ts
@@ -5,6 +5,8 @@ import { resizeImage } from "../utils/resizeImage";
 import { CustomError } from "@/types/customError";
 import Evaluation from "@/middleware/models/evaluation";
 import Image from "@/middleware/models/image";
+import { verifyToken } from "@/middleware/auth";
+import { requiredRole } from "@/middleware/roleChecker";
 
 const router = express.Router();
 /*
@@ -100,105 +102,102 @@ router.post(
 // Uusi mahdollinen evaluation tyyppi
 
 router.get("/all", async (_req, res: Response, next: NextFunction) => {
-    try {
-        const evaluations = await Evaluation.find();
+  try {
+    const evaluations = await Evaluation.find();
 
-        return res.json(evaluations);
-    } catch (error) {
+    return res.json(evaluations);
+  } catch (error) {
 
-        return next(new CustomError("Error fetching evaluations", 500));
-    }
+    return next(new CustomError("Error fetching evaluations", 500));
+  }
 });
 
 router.get("/:id", async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const evaluation = await Evaluation.findById(req.params.id);
+  try {
+    const evaluation = await Evaluation.findById(req.params.id);
 
-        if (!evaluation) {
-            throw new CustomError("Evaluation not found", 404);
-        }
-
-        return res.json(evaluation);
-    } catch (error) {
-        console.error("Error fetching image:", error);
-        return next(error);
+    if (!evaluation) {
+      throw new CustomError("Evaluation not found", 404);
     }
+
+    return res.json(evaluation);
+  } catch (error) {
+    console.error("Error fetching image:", error);
+    return next(error);
+  }
 });
 
 router.post(
-    "/save",
-    imageUploadHandler(),
-    async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            if (!req.file || !req.file.buffer) {
-                throw new CustomError("No image file provided", 400);
-            }
+  "/save",
+  verifyToken,
+  requiredRole("customer", "admin"),
+  imageUploadHandler(),
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      if (!req.file || !req.file.buffer) {
+        throw new CustomError("No image file provided", 400);
+      }
 
-            if (!req.body.user) {
-                throw new CustomError("User required", 400);
-            }
+      if (!req.user) {
+        throw new CustomError("User required", 400);
+      }
 
-            const optimizedImage = await resizeImage(req.file.buffer);
+      const optimizedImage = await resizeImage(req.file.buffer);
 
-            let userObject;
-            if (typeof req.body.user === "string") {
-                userObject = JSON.parse(req.body.user)
-            }
+      const imageForEvaluation = new Image({
+        contentType: req.file.mimetype,
+        image: optimizedImage.buffer,
+      });
 
-            const imageForEvaluation = new Image({
-                contentType: req.file.mimetype,
-                image: optimizedImage.buffer,
-            });
+      const savedImage = await imageForEvaluation.save();
+      console.log("saved image id: " + savedImage.id);
 
-            const savedImage = await imageForEvaluation.save();
-            console.log("saved image id: " + savedImage.id);
+      const newEvaluation = new Evaluation({
+        imageId: savedImage.id,
+        evaluation: {
+          brand: req.body.merkki || "Ei tiedossa",
+          model: req.body.malli || "Ei tiedossa",
+          color: req.body.vari || "Ei tiedossa",
+          dimensions: {
+            length: req.body.mitat?.pituus || 0,
+            width: req.body.mitat?.leveys || 0,
+            height: req.body.mitat?.korkeus || 0,
+          },
+          materials:
+            req.body.materiaalit || [],
+          condition: req.body.kunto || "Ei tiedossa",
+          user: req.user
+        },
+      });
 
-            const newEvaluation = new Evaluation({
-                imageId: savedImage.id,
-                evaluation: {
-                    brand: req.body.merkki || "Ei tiedossa",
-                    model: req.body.malli || "Ei tiedossa",
-                    color: req.body.vari || "Ei tiedossa",
-                    dimensions: {
-                        length: req.body.mitat?.pituus || 0,
-                        width: req.body.mitat?.leveys || 0,
-                        height: req.body.mitat?.korkeus || 0,
-                    },
-                    materials:
-                        req.body.materiaalit || [],
-                    condition: req.body.kunto || "Ei tiedossa",
-                    user: userObject,
-                },
-            });
-
-            const savedEvaluation = await newEvaluation.save();
-            return res.json(savedEvaluation);
-        } catch (error) {
-            console.error("Error saving evaluation", error);
-            return next(error);
-        }
+      const savedEvaluation = await newEvaluation.save();
+      return res.json(savedEvaluation);
+    } catch (error) {
+      console.error("Error saving evaluation", error);
+      return next(error);
     }
+  }
 );
 
 // Poistoreitti
 router.delete("/:id", async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const evaluation = await Evaluation.findById(req.params.id);
-        if (!evaluation) {
-            throw new CustomError("Evaluation not found", 404);
-        }
-
-        await Image.findByIdAndDelete(evaluation.imageId);
-
-        await Evaluation.findByIdAndDelete(req.params.id);
-
-        return res
-            .status(200)
-            .json({ message: "Evaluation and related image deleted successfully" });
-    } catch (error) {
-        console.error("Error deleting evaluation:", error);
-        return next(error);
+  try {
+    const evaluation = await Evaluation.findById(req.params.id);
+    if (!evaluation) {
+      throw new CustomError("Evaluation not found", 404);
     }
+
+    await Image.findByIdAndDelete(evaluation.imageId);
+
+    await Evaluation.findByIdAndDelete(req.params.id);
+
+    return res
+      .status(200)
+      .json({ message: "Evaluation and related image deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting evaluation:", error);
+    return next(error);
+  }
 });
 
 export default router;
